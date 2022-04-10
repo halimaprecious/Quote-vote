91da6f376a7ae0613e58cdcc9890d333b79e5fd0
halima precious <halimaprecious3@gmail.com>
2022-04-10 07:29:49 +0300

    add goal properties to goal class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,21 @@
 export class Quote {
+    id!:number;
+    showDetails!:boolean;
+    writer!:string;
+    quote!:string;
+    author!:string;
+    creationDate!:Date;
+    upvote!:number;
+    downvote!:number
+
+    constructor(id:number, writer:string, quote: string, author:string, creationDate:Date, upvote: number, downvote: number){
+        this.id =id;
+        this.writer= writer;
+        this.quote = quote;
+        this.author =author;
+        this.upvote = upvote;
+        this.creationDate =creationDate;
+        this.downvote = downvote;
+        this.showDetails =false;
+    }
 }
